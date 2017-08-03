306f64af35328b0c66183c1ef1c210fb97cc0e2e
Madalin Ignisca <madalin.ignisca@gmail.com>
2017-08-03 18:36:17 +0100

    M

--- README.Docker.md
+++ README.Docker.md
@@ -1,3 +1,11 @@
 # Using Docker as a development environment for Drupal projects
 
 Includes Nginx, PHP-FPM, MariaDB, Redis and Solr.
+
+## Getting started:
+
+* Have Docker installed, including `docker-compose`
+  * Recommended: use the official Docker installer if on macOS or Windows 10+
+    as they provide improved file sharing
+* in the project root, run `docker-compose up`
+* to stop hit `CTRL-C` and wait for the services to terminate (it's important)
--- docker/drupal/entrypoint.sh
+++ docker/drupal/entrypoint.sh
@@ -9,7 +9,15 @@ cd /app/web
 DRUPAL_INSTALLED="$(../vendor/bin/drush status bootstrap | grep -q Successful)"
 
 # If Drupal is not installed, do minimal installation
-if [ $DRUPAL_INSTALLED -eq 0 ]
+if [ -z "$DRUPAL_INSTALLED" ]
 then
-  echo "Drupal is not installed. Got: $DRUPAL_INSTALLED"
+  echo "Drupal is not installed.\n"
+  echo "Installing Drupal. This might take a minute.\n"
+  ../vendor/bin/drush si minimal -y --notify
+  echo "Drupal is now installed. Check the admin password above!\n"
 fi
+
+# exec CMD
+echo ">> exec docker CMD"
+echo "$@"
+exec "$@"
